f2d77579ffd63d7325790be52bcc17fc6fdde74b
manojdavuluri <manoj.davuluri18@gmail.com>
2023-12-04 15:44:34 -0500

    DI

---
+++ main.py
@@ -0,0 +1,15 @@
+from xRayClassifier import logger
+from xRayClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
---
+++ src/xRayClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,27 @@
+from xRayClassifier.config.configuration import ConfigurationManager 
+from xRayClassifier.components.data_ingestion import DataIngestion
+from xRayClassifier import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__=='__main__':
+        try:
+            config = ConfigurationManager()
+            data_ingestion_config = config.get_data_ingestion_config()
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.extract_zip_file()
+        except Exception as e:
+            raise e 
